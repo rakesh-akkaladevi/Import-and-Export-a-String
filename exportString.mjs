dd9c0eebd70090aa8147a1ef35174b795679e410
rakesh-akkaladevi <rakeshakkaladevi2011@gmail.com>
2022-04-30 15:05:09 +0000

    committed

---
+++ exportString.mjs
@@ -0,0 +1,3 @@
+// Write a string `Be Happy and Safe` in the file `exportString.mjs` and export it using the default export syntax.
+
+export default "Be Happy and Safe";
---
+++ importString.mjs
@@ -0,0 +1,2 @@
+import text1 from "./exportString.mjs";
+console.log(text1);
